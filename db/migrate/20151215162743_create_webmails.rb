71cfaa8816589d258130e8f5aac9a92efbbc1a43
Kai <wk1216123@126.com>
2015-12-16 22:59:03 +0800

    Revert "delete and change to rake scalfold"
    This reverts commit ec6d1285b94a5b698a196bb5d11479b2c0f4a095.

---
+++ db/migrate/20151215162743_create_webmails.rb
@@ -0,0 +1,14 @@
+class CreateWebmails < ActiveRecord::Migration
+  def change
+    create_table :webmails do |t|
+	t.integer :sender_id
+	t.string :sender_name
+	t.string :receiver_id
+	t.integer :receiver_type
+	t.string :string
+	t.integer :ifread
+
+	t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20151215162758_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+	t.belongs_to :article
+	t.integer :reply_id
+	t.string :content
+	t.integer :sender_id
+	t.integer :sender_type
+	t.timestamps null: false
+    end
+  end
+end
