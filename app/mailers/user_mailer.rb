9b13d2503fedb122741fade154a63ee20da1fc8d
Kai <wk1216123@126.com>
2015-12-20 00:21:10 +0800

    补上一些遗漏的文件

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url  = 'http://www.buaaclubs.com/api/users/email/verify/' + Digest::MD5.hexdigest("#{@user.email_verify_code.to_s}")
+    @url  = 'http://www.buaaclubs.com/api/users/email/verify/' + Digest::MD5.hexdigest("#{@user.email_verify_code.to_s + @user.stu_num}")
     mail(to: @user.email, subject: 'Welcome to BuaaClubs!')
   end
 
@@ -16,13 +16,13 @@ class UserMailer < ApplicationMailer
 
   def verify_email(user)
     @user = user
-    @url  = 'http://www.buaaclubs.com/api/users/email/verify/' + Digest::MD5.hexdigest("#{@user.email_verify_code.to_s}")
+    @url  = 'http://www.buaaclubs.com/api/users/email/verify/' + Digest::MD5.hexdigest("#{@user.email_verify_code.to_s + @user.stu_num}")
     mail(to: @user.email, subject: '您收到来自BuaaClubs的验证邮件！')
   end
 
   def fp_email(user)
     @user = user
-    @url  = 'http://www.buaaclubs.com/api/users/forgetpassword/email/verify/' + Digest::MD5.hexdigest("#{@user.email_verify_code.to_s}")
+    @url  = 'http://www.buaaclubs.com/api/users/forgetpassword/email/verify/' + Digest::MD5.hexdigest("#{@user.email_verify_code.to_s + @user.stu_num}")
     mail(to: @user.email, subject: '请确认您是否忘记密码！')
   end
 end
